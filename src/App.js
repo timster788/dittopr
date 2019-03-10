528add3c70fc2c2ac29bbb6adbf98310ca72b827
Timothy Dao <timster78811@gmail.com>
2019-03-10 14:28:12 -0400

    added page 3

--- src/App.js
+++ src/App.js
@@ -123,13 +123,9 @@ class App extends Component {
             isRequired: true
           },
           {
-            type: 'rating',
-            name: 'recommend friends',
-            visibleIf: '{satisfaction} > 3',
-            title:
-              'How likely are you to recommend the Product to a friend or co-worker?',
-            mininumRateDescription: 'Will not recommend',
-            maximumRateDescription: 'I will recommend'
+            type: 'comment',
+            name: 'Greatest Challenge',
+            title: 'What is Your Greatest Current Challenge?'
           },
           {
             type: 'comment',
@@ -142,40 +138,24 @@ class App extends Component {
       {
         questions: [
           {
-            type: 'radiogroup',
-            name: 'price to competitors',
-            title: 'Compared to our competitors, do you feel the Product is',
-            choices: [
-              'Less expensive',
-              'Priced about the same',
-              'More expensive',
-              'Not sure'
-            ]
+            type: 'comment',
+            name: 'How you do you see us helping you ',
+            title: 'How Do You See Us Helping You?'
           },
           {
-            type: 'radiogroup',
-            name: 'price',
-            title: 'Do you feel our current price is merited by our product?',
-            choices: [
-              'correct|Yes, the price is about right',
-              'low|No, the price is too low for your product',
-              'high|No, the price is too high for your product'
-            ]
+            type: 'comment',
+            name: 'results',
+            title: 'What Results Are You Expecting?'
           },
           {
-            type: 'multipletext',
-            name: 'pricelimit',
-            title: 'What is the... ',
-            items: [
-              {
-                name: 'mostamount',
-                title: 'Most amount you would every pay for a product like ours'
-              },
-              {
-                name: 'leastamount',
-                title: 'The least amount you would feel comfortable paying'
-              }
-            ]
+            type: 'comment',
+            name: 'goals',
+            title: 'What is Your Ultimate Goal for Your Business?'
+          },
+          {
+            type: 'comment',
+            name: 'success',
+            title: 'What would you consider a success or what is the goal?'
           }
         ]
       },
